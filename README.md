7e745107fb18756b7e31b413b548d2e06f18bdc9
Rathin <rathink4@gmail.com>
2023-01-16 22:41:55 -0600

    Added changes

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Python-Scripting
 
-A simple project to understand Python scripting.
+A scripting project to understand Python scripting.
 
 Project Steps/Requirements:
 
@@ -9,6 +9,6 @@ Project Steps/Requirements:
 - Copy and remove the "game" suffix of all games into the /games directory
 - Create a .json file with the information about the games
 - Compile all of the game code 
-- Run all of the game code-
+- Run all of the game code
 
 Python Scripting is done using the `get_game_data.py` file from the repository.
--- get_game_data.py
+++ get_game_data.py
@@ -1,9 +1,46 @@
+import json
 import os
 import sys
 import shutil
 from subprocess import PIPE, run
 
 GAME_DIRS_PATTERN = "game"
+GAME_EXTENSION = ".go"
+GAME_COMPILE_CMD = ["go", "build"]
+
+def createGameMetadataJson(json_path, game_dirs):
+    data = {
+        "gameNames": game_dirs,
+        "numberOfGames": len(game_dirs)
+    }
+
+    with open(json_path, "w") as f:
+        json.dump(data, f)
+
+
+def copyOver(source, dest):
+    # If there exist something in the dest, purge everything in it
+    if os.path.exists(dest):
+        shutil.rmtree(dest)
+    
+    # Copy over the content(s) from the source path to destination path
+    shutil.copytree(source, dest)
+
+def getGamesDirNames(paths, to_strip):
+    # Find the name of the game directories without the 'game' part in the names. Store and return the names of the game directories
+    # Eg = hello_world_game --> hello_world
+    names = []
+    for path in paths:
+        # In the path, find the dir_name only using the .split() function and then remove the _game section
+        _, dir_name = os.path.split(path)
+        game_dir_name = dir_name.replace(to_strip, "")
+        names.append(game_dir_name)
+    
+    return names
+
+def createDirectory(dest):
+    if not os.path.exists(dest):
+        os.mkdir(dest)
 
 def getAllGamePaths(source):
     # Basically get a list of all the paths to the games
@@ -15,18 +52,64 @@ def getAllGamePaths(source):
             # check if the directories in (source='data') contains the word 'game' in it, then append to list
             if GAME_DIRS_PATTERN in directory.lower():
                 path = os.path.join(source, directory)
-                game_path.append()
+                game_path.append(path)
         
         break
 
     return game_path
 
+def compileGame(path):
+    # Find the game file (game.go) and compile it using 'go build game.go' command
+    game_file = None
+
+    for root, dirs, files in os.walk(path):
+        # source = any game directory (eg. 'hello_world') and since only the files in these directory is concerned, 
+        # we break through the loop immediately after 1 iteration
+        for file in files:
+            if file.endswith(GAME_EXTENSION):
+                game_file = file
+                break
+        
+        break
+
+    # Check if we actually found a file
+    if game_file is None:
+        return
+    
+    command = GAME_COMPILE_CMD + [game_file]
+    runGame(command, path)
+
+def runGame(cmd, path):
+    # Change over to the game directory and then run the command
+    cwd = os.getcwd()
+    os.chdir(path)
+
+    results = run(cmd, stdout=PIPE, stdin=PIPE, universal_newlines=True)
+    print(results)
+
+    # Change back to the cwd to avoid any issues
+    os.chdir(cwd)
+
 def main(source, dest):
     # Get the current working directory and create the path to source & target
     cwd = os.getcwd()
     source_path = os.path.join(cwd, source)
-    dest_path = os.path.joint(cwd, dest)
+    dest_path = os.path.join(cwd, dest)
+
+    game_paths = getAllGamePaths(source_path)
+    game_dirs_name = getGamesDirNames(game_paths, "_game")
+
+    createDirectory(dest_path)
     
+    for source, dest in zip(game_paths, game_dirs_name):
+        # You create destination_path = (cwd)/target/hello_world
+        # Then you copy the contents from (cwd)/data/hello_world_game to cwd/target/hello_world
+        destination_path = os.path.join(dest_path, dest)
+        copyOver(source, destination_path)
+        compileGame(destination_path)
+    
+    json_path = os.path.join(dest_path, "gameMetaData.json")
+    createGameMetadataJson(json_path, game_dirs_name)
 
 
 if __name__ == '__main__':
